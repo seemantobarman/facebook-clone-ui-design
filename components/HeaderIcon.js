01d291ad5c1eb076a41a90174fc2f697b3c3c77d
Seemanto Barman <seemanto@hotmail.com>
2021-07-21 04:00:12 +0600

    initial commit

---
+++ components/HeaderIcon.js
@@ -0,0 +1,14 @@
+function HeaderIcon(props) {
+    const { Icon, active } = props;
+    return (
+        <div className="group cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 flex items-center rounded-xl md:active:border-b-2 md:active:border-blue-500">
+            <Icon
+                className={`h-5 text-gray-500 md:h-7 group-hover:text-blue-500 text-center ${
+                    active && "text-blue-500"
+                }`}
+            />
+        </div>
+    );
+}
+
+export default HeaderIcon;
